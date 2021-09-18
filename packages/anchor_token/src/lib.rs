c0e6e9f0e944f6b000ee3512550d04edcf6172ef
lumiere <spectrum.lumiere@gmail.com>
2021-09-18 11:00:58 +0700

    update version

--- packages/anchor_token/src/lib.rs
+++ packages/anchor_token/src/lib.rs
@@ -4,5 +4,12 @@ pub mod common;
 pub mod community;
 pub mod distributor;
 pub mod gov;
+pub mod querier;
 pub mod staking;
 pub mod vesting;
+
+#[cfg(test)]
+mod mock_querier;
+
+#[cfg(test)]
+mod testing;
