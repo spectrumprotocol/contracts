d8945cd331ab3716c1223707ed263b3ed94fd9e4
lumiere <spectrum.lumiere@gmail.com>
2021-12-06 17:36:47 +0700

    run schema all

--- schema_all.sh
+++ schema_all.sh
@@ -1,8 +1,19 @@
 cd contracts
+cd core
 dirs=($(find . -maxdepth 1 -type d \( ! -name . \)))
 for dir in "${dirs[@]}"; do
   cd $dir
   cargo schema
   cd ..
 done
+cd ..
+
+cd farms
+dirs=($(find . -maxdepth 1 -type d \( ! -name . \)))
+for dir in "${dirs[@]}"; do
+  cd $dir
+  cargo schema
+  cd ..
+done
+cd ..
 cd ..
