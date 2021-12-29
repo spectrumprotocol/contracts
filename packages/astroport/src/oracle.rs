1832a2ed92df81bbdbc4f95636db355100380ca0
SpectrumRhealward <84844988+SpectrumRhealward@users.noreply.github.com>
2021-12-29 16:50:17 +0700

    Revert "astroport packages 0.3.1 upgrade and generator_to_proxy template files"
    This reverts commit c52948d36bb979897f3d85fa1f7e63eda9834c12.

--- packages/astroport/src/oracle.rs
+++ packages/astroport/src/oracle.rs
@@ -3,18 +3,12 @@ use cosmwasm_std::Uint128;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-/// ## Description
-/// This structure describes the basic settings for creating a contract.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
-    /// the factory contract address
     pub factory_contract: String,
-    /// the type of asset infos available in [`AssetInfo`]
     pub asset_infos: [AssetInfo; 2],
 }
 
-/// ## Description
-/// This structure describes the execute messages of the contract.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
@@ -22,22 +16,12 @@ pub enum ExecuteMsg {
     Update {},
 }
 
-/// ## Description
-/// This structure describes the query messages of the contract.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    /// Validates assets and calculates a new average amount with updated precision
-    Consult {
-        /// the assets to validate
-        token: AssetInfo,
-        /// the amount
-        amount: Uint128,
-    },
+    /// Consult
+    Consult { token: AssetInfo, amount: Uint128 },
 }
 
-/// ## Description
-/// This structure describes a migration message.
-/// We currently take no arguments for migrations.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct MigrateMsg {}
