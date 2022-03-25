41fad02886c78bc385201c2b9c6d1b2231436abd
SpectrumRhealward <84844988+SpectrumRhealward@users.noreply.github.com>
2022-03-26 00:18:59 +0700

    weldo_stluna farm

---
+++ contracts/astro_farms/spectrum_astroport_weldo_stluna_farm/examples/schema.rs
@@ -0,0 +1,22 @@
+use std::env::current_dir;
+use std::fs::create_dir_all;
+
+use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
+use spectrum_protocol::astroport_stluna_luna_farm::{
+    ConfigInfo, Cw20HookMsg, ExecuteMsg, PoolsResponse, QueryMsg, RewardInfoResponse, StateInfo,
+};
+
+fn main() {
+    let mut out_dir = current_dir().unwrap();
+    out_dir.push("schema");
+    create_dir_all(&out_dir).unwrap();
+    remove_schemas(&out_dir).unwrap();
+
+    export_schema(&schema_for!(ExecuteMsg), &out_dir);
+    export_schema(&schema_for!(Cw20HookMsg), &out_dir);
+    export_schema(&schema_for!(QueryMsg), &out_dir);
+    export_schema(&schema_for!(ConfigInfo), &out_dir);
+    export_schema(&schema_for!(RewardInfoResponse), &out_dir);
+    export_schema(&schema_for!(PoolsResponse), &out_dir);
+    export_schema(&schema_for!(StateInfo), &out_dir);
+}
