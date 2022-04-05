3848c3ce6869af89336b982a6922eff2cdca4cfc
SpectrumRhealward <84844988+SpectrumRhealward@users.noreply.github.com>
2022-04-05 22:57:44 +0700

    test bond, incomplete test compound for stluna-luna, weldo-stluna

--- contracts/astro_farms/spectrum_astroport_stluna_luna_farm/src/lib.rs
+++ contracts/astro_farms/spectrum_astroport_stluna_luna_farm/src/lib.rs
@@ -6,13 +6,10 @@ pub mod state;
 pub mod model;
 
 #[cfg(test)]
-mod tests_bond;
-
-#[cfg(test)]
 mod tests_bond_without_gov_proxy;
 
-#[cfg(test)]
-mod tests_compound;
+// #[cfg(test)]
+// mod tests_compound;
 
 #[cfg(test)]
 mod mock_querier;
