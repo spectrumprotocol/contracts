b7c880fc702e59451c875e9c731cb9e90f7f65e8
SpectrumRhealward <84844988+SpectrumRhealward@users.noreply.github.com>
2022-05-25 12:48:38 +0700

    unblocked token with migrate

--- contracts/spectrum_token/src/contract.rs
+++ contracts/spectrum_token/src/contract.rs
@@ -26,13 +26,7 @@ pub fn execute(
     info: MessageInfo,
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
-    match msg {
-        ExecuteMsg::Send { .. } => { Err(ContractError::Unauthorized {}) },
-        ExecuteMsg::SendFrom { .. } => { Err(ContractError::Unauthorized {}) },
-        ExecuteMsg::Transfer { .. } => { Err(ContractError::Unauthorized {}) },
-        ExecuteMsg::TransferFrom { .. } => { Err(ContractError::Unauthorized {}) },
-        _ => cw20_execute(deps, env, info, msg),
-    }
+    cw20_execute(deps, env, info, msg)
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
